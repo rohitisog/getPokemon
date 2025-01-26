b0c1e5652d6707e0bb8d139f31f4156e955d9ef2
rohitverse <rohitkr23072001@gmail.com>
2025-01-27 00:49:39 +0530

    adding pokemon card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router";
+import PokemonDetailsPage from "./pages/PokemonDetailsPage";
+import SearchPage from "./pages/SearchPage";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   return (
     <>
+      <Link to={"/"}> Home </Link>
+      <Link to={"/pages/SearchPage"}> Search Page </Link>
+      <Link to={"/pages/PokemonDetailsPage"}> Pokemon Details Page </Link>
       <Routes>
         <Route path={"/"} element={""}></Route>
-        <Route path={"/pages/"} element={""}></Route>
-
+        <Route
+          path="/pages/PokemonDetailsPage"
+          element={<PokemonDetailsPage />}
+        ></Route>
+        <Route path="/pages/SearchPage" element={<SearchPage />}></Route>
+        <Route path="/*" element={<PageNotFound />}></Route>
       </Routes>
     </>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const PokemonCard = () => {
-  return (
-    <div>PokemonCard</div>
-  )
-}
 
-export default PokemonCard
+    
+  return (
+    <>
+      <div className="pokemonCard">
+        <img src="" alt="" />
+        <h2> Pokemon Name </h2>
+        <p> Pokemon HP </p>
+      </div>
+    </>
+  );
+};
+
+export default PokemonCard;
+
+{
+  /* Type
+        HP
+        Attack
+        Defense
+        Special Attack 
+        Special Defense
+        Speed */
+}
